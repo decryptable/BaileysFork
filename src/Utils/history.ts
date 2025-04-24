12bddf4e294a937d9abcbea5f74008f76311e082
decryptable <decryptable1337@gmail.com>
2025-04-24 19:18:40 +0000

    fix: ensure buffer is correctly typed after decompression in downloadHistory function

--- src/Utils/history.ts
+++ src/Utils/history.ts
@@ -23,7 +23,7 @@ export const downloadHistory = async(
 	let buffer = Buffer.concat(bufferArray)
 
 	// decompress buffer
-	buffer = await inflatePromise(buffer)
+	buffer = await inflatePromise(buffer) as Buffer
 
 	const syncData = proto.HistorySync.decode(buffer)
 	return syncData
